bd4b42b3d14604191c728d6cd27e9ec0dca77c2b
wood-zachary <zwoodcollege@gmail.com>
2026-09-18 19:20:38 -0700

    Add synchronizer for async kp_col input

--- labs/lab3/fpga/src/lab3_zw.sv
+++ labs/lab3/fpga/src/lab3_zw.sv
@@ -1,22 +1,32 @@
 module lab3_zw(
-	input  logic 	   rst_n,	// active-low push button (internal pull-up) for reset
-	input  logic [3:0] kp_col,	// asynchronous keypad column inputs (pulled up)
+	input  logic 	   rst_n,			// active-low push button (internal pull-up) for reset
+	input  logic [3:0] kp_col_async,	// asynchronous keypad column inputs (pulled up)
 
-	output logic [6:0] seg,		// shared segment lines, time multiplexed
-	output logic [1:0] an_en,	// anode enable outputs for time multiplexing digits
-	output logic [3:0] kp_row	// asserted row to scan keypad
+	output logic [6:0] seg,				// shared segment lines, time multiplexed
+	output logic [1:0] an_en,			// anode enable outputs for time multiplexing digits
+	output logic [3:0] kp_row			// asserted row to scan keypad
 );
 
 	logic int_osc;
 	logic sel;
 	logic [15:0] mux_count;
 	logic [3:0] digit_data;
+	logic [3:0] kp_col_sync;
 
 	// Internal high-speed oscillator generating a 48 MHz clock
 	HSOSC hf_osc (
 		.CLKHFPU(1'b1),
 		.CLKHFEN(1'b1),
 		.CLKHF(int_osc)
+	);
+
+	// Synchronizer for kp_col inputs
+	// kp_col is four separate inputs packed together, so an async FIFO shouldn't be necessary
+	synchronizer #(.WIDTH(4), .STAGES(2)) sync (
+		.clk(int_osc),
+		.rst(~rst_n),
+		.data_in(kp_col_async),
+		.data_out(kp_col_sync)
 	);
 
 	// Multiplexing counter for toggling the active digit at 1 kHz
---
+++ labs/lab3/fpga/src/synchronizer.sv
@@ -0,0 +1,22 @@
+module synchronizer #(
+    parameter int WIDTH = 1,
+    parameter int STAGES = 2,
+    parameter logic [WIDTH-1:0] INIT_VAL = 0
+) (
+    input  logic clk,
+    input  logic rst,
+    input  logic data_in,
+
+    output logic data_out
+);
+
+    logic [STAGES-1:0][WIDTH-1:0] sync_regs;
+
+    always_ff @(posedge clk, posedge rst) begin
+        if (rst) sync_regs <= INIT_VAL;
+        else sync_regs <= {sync_regs[STAGES-2:0], data_in};
+    end
+
+    assign data_out = sync_regs[STAGES-1];
+
+endmodule
